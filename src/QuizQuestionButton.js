5ecc5875c56fba6ccacb68655312ba86d4960be6
James <james.randolph2016@gmail.com>
2018-10-01 19:25:54 -0400

    buttondisplayed

--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react'
 
 class QuizQuestionButton extends Component {
-
+    render() {
+        return (
+            <li><button>{this.props.button_tex}t</button></li>
+        )
+    }
 }
 
 export default QuizQuestionButton
