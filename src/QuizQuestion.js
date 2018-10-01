35ca8635dd6cd33dea1f3bd118293585327fcf95
James <james.randolph2016@gmail.com>
2018-10-01 19:20:27 -0400

    shos list item tag and question

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -5,10 +5,11 @@ class QuizQuestion extends Component {
         return (
             <main>
                 <section>
-                    <p>text goes here</p>
+                    <p>{this.props.quiz_question.instruction_text}</p>
                 </section>
                     <section className="buttons">
                     <ul>
+                        <li>{this.props.quiz_question.answer_options[0]}</li>
                     </ul>
                 </section>
             </main>
