1b0c0c881c46bbf6f063dc9ee2d7bbe0288c6ff7
James <james.randolph2016@gmail.com>
2018-10-01 19:16:52 -0400

    reutnrn method

---
+++ src/QuizQuestion.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+class QuizQuestion extends Component {
+    render () {
+        return (
+            <main>
+                <section>
+                    <p>text goes here</p>
+                </section>
+                    <section className="buttons">
+                    <ul>
+                    </ul>
+                </section>
+            </main>
+        )
+    }
+}
+
+export default QuizQuestion
