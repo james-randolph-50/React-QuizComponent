31acf293cf38b88b1dbfe094988d0bc0f02dbfab
James <james.randolph2016@gmail.com>
2018-10-02 20:35:36 -0400

    typo

--- src/Quiz.js
+++ src/Quiz.js
@@ -18,7 +18,7 @@ class Quiz extends Component {
         this.setState({ quiz_position: 1 })
     }
     render() {
-        const isQuizEnd = ((this.state.quiz_position -1) === quizData.quiz_questions.length)
+        const isQuizEnd = ((this.state.quiz_position - 1) === quizData.quiz_questions.length)
         return (
             <div>
                 {isQuizEnd ? <QuizEnd resetClickHanlder={this.handleResetClick.bind(this)} /> : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} showNextQuestionHandler={this.showNextQuestion.bind(this)}/>}
